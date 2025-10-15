044a6073e330d97a3519ee6fb6306953ccc0613c
Fabota51 <fabotina@gmail.com>
2025-10-15 09:39:41 +0200

    Azure deploy test11

--- Azure/start.sh
+++ Azure/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-kubectl apply -f .\00_NameSpace.yaml
-kubectl apply -f .\01_PV-PVC.yaml
-kubectl apply -f .\02_Postgress.yaml
+kubectl apply -f ./00_NameSpace.yaml
+kubectl apply -f ./01_PV-PVC.yaml
+kubectl apply -f ./02_Postgress.yaml
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.19.0/cert-manager.yaml
 timeout 10
-kubectl apply -f .\03_Django.yaml
-kubectl apply -f .\04_Ingress.yaml
+kubectl apply -f ./03_Django.yaml
+kubectl apply -f ./04_Ingress.yaml
